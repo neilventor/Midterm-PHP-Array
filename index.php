e5a7ccb25ee5243cae33b898f4c5ec619d2804ee
Nathaniel Ventor <neilventor@yahoo.com>
2019-02-04 14:39:43 +0800

    added codes to ouput the array

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    $age=array("Peter"=>"35","Ben"=>"37","Joe"=>"43");
+        foreach($age as $name=>$age_value){
+            echo "Name: ".$name.", Age: ".$age_value;
+            echo "<br>";
+            echo "<br>";
+            }
+?>
